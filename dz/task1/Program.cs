b767a3e7e9353b516bbf804ab094196205dd5fc3
Euphoria <polunine28@gmail.com>
2022-11-29 17:11:34 +1100

    task1

---
+++ dz/task1/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Write("Введите трехзначное число:");
+int numbers = Convert.ToInt32(Console.ReadLine());
+string stringnumbers = Convert.ToString(numbers);
+Console.WriteLine("Вторая цифра числа > "+stringnumbers[1]);
